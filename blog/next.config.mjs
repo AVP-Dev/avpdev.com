1d964f95186c60ab8f1fdb3077933ac81a0a8d8a
AVP <avp@MacBook-Air-2.local>
2025-08-12 00:13:08 +0300

    feet: откат

--- blog/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    // Включаем строгий режим React для выявления потенциальных проблем
-    reactStrictMode: true,
-    // Настройка для вывода автономного приложения, оптимизированного для Docker
-    output: 'standalone',
-};
-
-export default nextConfig;
